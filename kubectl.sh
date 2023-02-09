bf329237ad753afe13a9ee7e0513b902c1b22353
Bhavesh Muleva <bhaveshmuleva503@gmail.com>
2023-02-09 15:11:28 +0530

    first commit

---
+++ kubectl.sh
@@ -0,0 +1,16 @@
+#Update the apt package index and install packages needed to use the Kubernetes apt repository:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+
+#If you use Debian 9 (stretch) or earlier you would also need to install apt-transport-https
+sudo apt-get install -y apt-transport-https
+
+#Download the Google Cloud public signing key:
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+#Add the Kubernetes apt repository:
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#Update apt package index with the new repository and install kubectl:
+sudo apt-get update
+sudo apt-get install -y kubectl
