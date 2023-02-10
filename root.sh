9c82eee8212aeb02c140a2d3f403cebd1cfc69d4
Bhavesh Muleva <66737092+Bhavesh-Muleva@users.noreply.github.com>
2023-02-10 21:18:32 +0530

    Update root.sh

--- root.sh
+++ root.sh
@@ -12,3 +12,17 @@ echo "Kubectl, Kind, Clusterctl, Clusterawsadm Installed"
 echo "---"
 kind create cluster
 echo "Cluster is Created..."
+
+echo "Time to create management cluster"
+# Enable the experimental Cluster topology feature.
+export CLUSTER_TOPOLOGY=true
+
+# Initialize the management cluster
+clusterctl init --infrastructure docker
+
+echo "generating capi-quickstart.yaml file"
+clusterctl generate cluster capi-quickstart --flavor development \
+  --kubernetes-version v1.26.0 \
+  --control-plane-machine-count=1 \
+  --worker-machine-count=2 \
+  > capi-quickstart.yaml
