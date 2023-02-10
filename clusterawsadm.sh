20b2a4416f82e2828b8f6f4bca2ac3ddbfac167f
Bhavesh Muleva <bhaveshmuleva503@gmail.com>
2023-02-10 10:10:02 +0530

    'clusterawsadm'

---
+++ clusterawsadm.sh
@@ -0,0 +1,7 @@
+curl -L https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v2.0.2/clusterawsadm-linux-amd64 -o clusterawsadm
+
+chmod +x clusterawsadm
+
+sudo mv clusterawsadm /usr/local/bin
+
+clusterawsadm version
